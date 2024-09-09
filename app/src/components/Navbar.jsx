3fdfe1a2a1c9ed9085bfb17237215d1b7687977e
Jakub Serok <83843596+jakubSerok@users.noreply.github.com>
2024-09-09 17:50:25 +0200

    navbar
    Add moblie menu for navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,34 +1,105 @@
-import React, { useEffect, useState } from "react";
-
-import logo from "../assets/logo.png"
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  let [open, setOpen] = useState(false);
 
-    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
-
-    const toggleNavbar = () => {
-        setMobileDrawerOpen(!mobileDrawerOpen);
-      };
-    
   return (
-    <nav className="w-full h-[140px] flex flex-col">
-        <div className="lg:hidden">
-            <div className='h-[60%] border-b-2 border-black flex justify-between px-6 items-center'>
-                <img src={logo} alt="" className='w-[100px] h-full'/>
-                <div>2</div>
-            </div>
-            <div className='h-[40%] items-center justify-between flex px-4'>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>sklep</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>home</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>blogi</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>o nas</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>kontakt</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>oferta</div>
-                <div className='md:text-[20px] sm:text-[15px] text-black bold uppercase'>nasze rozwiazania</div>
-            </div>
+    <nav className="w-full h-[140px] ">
+      {/* Desktop Menu */}
+      <div className="hidden sm:flex flex-col h-full">
+        <div className="h-[70%] border-b-2 border-black flex justify-between px-6 items-center">
+          <img src={logo} alt="" className="w-[100px] h-full" />
+          <div>2</div>
         </div>
-    </nav>
-  )
-}
 
-export default Navbar
+        <ul className="flex items-center justify-between px-4 h-[70%]">
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">sklep</a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">home</a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">blogi</a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">o nas </a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">kontakt</a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">oferta</a>
+          </li>
+          <li className="md:text-[20px] sm:text-[15px] text-black bold uppercase">
+            <a href="">nasze rozwiazania</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="sm:hidden h-full">
+        {/* Menu button */}
+        <div className="flex justify-between items-center h-full px-5">
+          <img src={logo} alt="Logo" className="w-[100px] h-full" />
+          <div onClick={() => setOpen(!open)}>
+            {open ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+          </div>
+        </div>
+
+        {/* Sliding Mobile Menu */}
+        <div
+          className={`fixed top-0 left-0 w-[75%]  bg-white z-50 p-5 transition-transform duration-500 ${
+            open ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          {/* Logo at the top of the mobile menu */}
+          <img src={logo} alt="Logo" className="w-[100px] h-auto mb-6" />
+
+          {/* Menu items */}
+          <ul className="flex flex-col items-start justify-center h-full space-y-4">
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                sklep
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                home
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                blogi
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                o nas
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                kontakt
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                oferta
+              </a>
+            </li>
+            <li className="text-[20px] text-black bold uppercase border-b-2 w-full">
+              <a href="" className="w-full block py-2">
+                nasze rozwiazania
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
