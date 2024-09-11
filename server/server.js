e6cc6b951abb44a4619e3ef514fc705bed621850
Jakub Serok <83843596+jakubSerok@users.noreply.github.com>
2024-09-11 12:32:58 +0200

    backednd update
    Add product shames and endpoits for them

--- server/server.js
+++ server/server.js
@@ -1,14 +1,142 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend is running');
+const uri =
+  "mongodb+srv://admin:Nimda209$@cluster0.ku8q9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Start the Express.js server
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
+    });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//Creating Upload Endpoin for images
+app.use("/images", express.static("upload/images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    succes: 1,
+    image_url: `http://localhost:${3001}/images/${req.file.filename}`,
+  });
+});
+
+//Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+//Creating Endpoint for adding product
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.new,
+  });
+  await product.save();
+  res.json({
+    succes: true,
+    name: req.body.name,
+  });
+});
+
+//Creating API for deleting product
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  res.json({
+    succes: true,
+    name: req.body.name,
+  });
+});
+
+//Creating API for getting all prdocuts
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  res.send(products);
 });
