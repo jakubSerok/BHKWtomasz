62e486ad896a66f862f6ed7078950f2653881323
Jakub Serok <83843596+jakubSerok@users.noreply.github.com>
2024-09-30 22:44:27 +0200

    Fix
    fix backend, and some basice erros

--- server/server.js
+++ server/server.js
@@ -9,8 +9,22 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 //Database Connection with MonoDB
 mongoose.connect(
