83ba2b594102c7ea145226c6706336b7b3187ced
Jakub Serok <83843596+jakubSerok@users.noreply.github.com>
2024-09-30 22:51:47 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,13 +11,13 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://bhkwtomasz.vercel.app"],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://bhkwtomasz.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
